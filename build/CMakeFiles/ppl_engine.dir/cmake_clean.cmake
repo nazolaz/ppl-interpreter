63af8f78310b49410154c7ce3adc7360626a163c
nazolaz <Nazareno_Olaz@hotmail.com>
2026-07-11 00:50:15 +0000

    ended PPLRunner for interpreter to be runned in terminal + wrote some generative models for testing

---
+++ build/CMakeFiles/ppl_engine.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ppl_engine.dir/src/Distribution.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/Distribution.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/HOPPLParser.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/HOPPLParser.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/LikelihoodWeighting.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/LikelihoodWeighting.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/Machine.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/Machine.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/PPLRunner.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/PPLRunner.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/Primitives.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/Primitives.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/SSMetropolisHastings.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/SSMetropolisHastings.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/SequentialMonteCarlo.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/SequentialMonteCarlo.cpp.o.d"
+  "CMakeFiles/ppl_engine.dir/src/main.cpp.o"
+  "CMakeFiles/ppl_engine.dir/src/main.cpp.o.d"
+  "ppl_engine"
+  "ppl_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ppl_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
